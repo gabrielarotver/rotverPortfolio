f181d6901891a6c1fe9c031d53908412516717f0
Gabriela Rotver <gabrielarotver86@gmail.com>
2016-09-07 15:16:42 -0400

    added smooth scrolling, added button, some more styling

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+  // mobile nav bar
+  $(".button-collapse").sideNav();
+
+  // smooth scrooling
+  $(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 3000);
+        return false;
+      }
+    }
+  });
+});
+});
