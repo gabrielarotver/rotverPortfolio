4c195823dad064b858f244c03f9069be70a4b126
Gabriela Rotver <gabrielarotver86@gmail.com>
2016-09-08 14:52:19 -0400

    got flexslider to work and added a card and a gridsystem for each slide

--- js/index.js
+++ js/index.js
@@ -1,21 +1,32 @@
 $(document).ready(function(){
 
-  // mobile nav bar
-  $(".button-collapse").sideNav();
+  // flexslider
+  $('.flexslider').flexslider({
+    animation: "slide",
+		directionNav: false,
+		touch: true,
+		pauseOnHover: true,
+		start: function() {
+			$.waypoints('refresh');
+		}
+  });
 
   // smooth scrooling
   $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 2000);
-        return false;
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 2000);
+          return false;
+        }
       }
-    }
+    });
   });
-});
+
+  // mobile nav bar
+  $(".button-collapse").sideNav();
 });
